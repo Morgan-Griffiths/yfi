2490ea141e27e0f650b581d6dd4231ec2ac4c581
Morgan <c5ipo7i@yahoo.com>
2021-02-07 18:12:48 -0800

    chore:reverted hardhat for CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,41 +6,32 @@ require('@nomiclabs/hardhat-truffle5');
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
 require('@nomiclabs/hardhat-waffle');
-
-const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
-  solidity: {
-    compilers: [{ version: '0.6.12', settings: {} }]
-  },
-  networks: {
-    hardhat: {},
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ACCESS_TOKEN}`,
-      accounts: [`${process.env.TEST_PRIVATE_KEY}`]
-    }
-  },
-  paths: {
-    sources: './contracts',
-    tests: './test',
-    cache: './cache',
-    artifacts: './artifacts'
-  }
-  // etherscan: {
-  //   // Your API key for Etherscan
-  //   // Obtain one at https://etherscan.io/
-  //   apiKey: ETHERSCAN_API_KEY
-  // }
+module.exports = {
+  solidity: '0.6.12'
 };
-
-export default config;
-// import { HardhatUserConfig } from "hardhat/types";
-// const config: HardhatUserConfig = {};
-// export default config;
-// module.exports = {
-//   networks: {
-//     mainnet: { ... }
+// const config: HardhatUserConfig = {
+//   defaultNetwork: 'hardhat',
+//   solidity: {
+//     compilers: [{ version: '0.6.12', settings: {} }]
 //   },
-//   etherscan: {
-//     apiKey: etherscanApiKey
+//   networks: {
+//     hardhat: {},
+//     rinkeby: {
+//       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ACCESS_TOKEN}`,
+//       accounts: [`${process.env.TEST_PRIVATE_KEY}`]
+//     }
+//   },
+//   paths: {
+//     sources: './contracts',
+//     tests: './test',
+//     cache: './cache',
+//     artifacts: './artifacts'
 //   }
+//   // etherscan: {
+//   //   // Your API key for Etherscan
+//   //   // Obtain one at https://etherscan.io/
+//   //   apiKey: ETHERSCAN_API_KEY
+//   // }
 // };
+
+// export default config;
