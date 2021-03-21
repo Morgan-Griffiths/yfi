78665e97f4a879f9895f7ef9762a9c42259df9fc
Morgan <c5ipo7i@yahoo.com>
2021-03-20 17:15:57 -0700

    chore: added abi extractor

---
+++ scripts/get_contract_abi.js
@@ -0,0 +1,15 @@
+import ethers from 'ethers';
+var fs = require('fs');
+
+const stratContract = await ethers.getContractFactory('Voting');
+const BFIToken = await ethers.getContractFactory('BFIToken');
+const contract = new ethers.Contract(token.address, token.interface, owner);
+fs.writeFileSync(
+  'voting_abi.json',
+  stratContract.interface.format('json'),
+  function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  }
+);
