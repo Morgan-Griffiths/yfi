6dc1e38319275200c82b0be7034709703416b693
Morgan <c5ipo7i@yahoo.com>
2021-03-26 11:46:52 -0700

    chore: stand alone script for extracting abis

--- scripts/get_contract_abi.js
+++ scripts/get_contract_abi.js
@@ -1,24 +1,27 @@
-import ethers from 'ethers';
+const { ethers } = require('hardhat');
 var fs = require('fs');
 
-const stratContract = await ethers.getContractFactory('Voting');
-const BFIToken = await ethers.getContractFactory('BFIToken');
-const contract = new ethers.Contract(token.address, token.interface, owner);
-fs.writeFileSync(
-  'voting_abi.json',
-  stratContract.interface.format('json'),
-  function (err) {
-    if (err) {
-      return console.error(err);
+async function main() {
+  const stratContract = await ethers.getContractFactory('Voting');
+  const BFIToken = await ethers.getContractFactory('BFIToken');
+  fs.writeFileSync(
+    'voting_abi.json',
+    stratContract.interface.format('json'),
+    function (err) {
+      if (err) {
+        return console.error(err);
+      }
     }
-  }
-);
-fs.writeFileSync(
-  'token_abi.json',
-  BFIToken.interface.format('json'),
-  function (err) {
-    if (err) {
-      return console.error(err);
+  );
+  fs.writeFileSync(
+    'token_abi.json',
+    BFIToken.interface.format('json'),
+    function (err) {
+      if (err) {
+        return console.error(err);
+      }
     }
-  }
-);
+  );
+}
+
+main();
